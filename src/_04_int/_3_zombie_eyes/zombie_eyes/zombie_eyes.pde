b4cc1d315af22415f2a8c0c65c1281c32c6fc7f7
root <root@03963a879c0a>
2023-01-22 20:58:21 +0000

    i did zombie eyes and my ages today

--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,18 +1,18 @@
 
 void setup() {
-PImage face = loadImage("face.jpg");
-size(600,600);
-face.resize(600,600);
-image(face,0,0);
 
+  PImage face = loadImage("face.jpg");
+  size(600,600);
+  face.resize(600,600);
+  image(face,0,0);
 }
 
 void draw() {
-fill(mouseX,mouseY,50);
-ellipse(380,380,150,200);
+  fill(mouseX/2,mouseY/2,(mouseX+mouseY)/2);
 ellipse(225,380,150,200);
+ellipse(380,380,150,200);
 fill(#000000);
-ellipse(200,400,30,30);
-ellipse(400,400,30,30);
+ellipse(380,380,50,50);
+ellipse(225,380,50,50);
 
 }
--- src/_05_for_loops/_1_my_ages/MyAges.java
+++ src/_05_for_loops/_1_my_ages/MyAges.java
@@ -10,17 +10,21 @@ public class MyAges {
 		//        Start its value at zero.
 		
 		// Change the value of yourCurrentAge to match your age
-		int yourCurrentAge = 0;
+		int yourCurrentAge = 10;
 				
 		
 		//  WHILE LOOP. Start a while loop to repeat while age <= yourCurrentAge
+				int age = 0;
+				while(age<=10) {
+					System.out.println(age);
+					age++;
 				
 			//  Print the age variable
-		
+				
 			//  Add one to the age variable
 		
 		//  End the while loop here
-
+				}
 		//  Run the program. Did it print all your ages from 0 to your current age?
 		
 		/*** Now you are going to do the same thing, but this time use a for loop. 
@@ -30,11 +34,12 @@ public class MyAges {
 		
 		//  FOR LOOP. Start a for loop to repeat while age <= your age, like this:
 		//  for ( age=0; age <= yourAge; age +=1 ) {
-		
+		for(age=0; age <= 10; age+=1) {
 			//  Print the age variable
-		
+		System.out.println(age);
+		age++;
 		//  End the for loop here
-		
+		}
 		//  Run the program again. 		
 		//  Check that both loops printed the same numbers to the console
 	}
