301e40906f23591cf415a1665810358e541ba0bc
root <root@f1d0342bd1d8>
2023-01-26 02:25:59 +0000

    i did the badger song today

---
+++ src/_05_for_loops/_2_badgers/Badger_song.java
@@ -0,0 +1,21 @@
+package _05_for_loops._2_badgers;
+
+
+
+
+public class Badger_song {
+public static void main(String[] args) {
+	
+	for(int i=0;i<12;i++) {
+		System.out.print("badger,");
+	}
+	for(int i=0;i<2;i++) {
+		System.out.println("mushroom,");
+	}
+	
+	
+	for(int i=0;i<1;i++) {
+		System.out.println(" A snake!");
+	}
+}
+}
